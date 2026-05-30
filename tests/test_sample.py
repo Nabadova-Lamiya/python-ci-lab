1b5c46ca1505f27dc1ae5acb68d8001fc7276836
Lamiya Nabadova <nabadova@Lamiyas-MacBook-Air.local>
2026-05-30 15:37:29 +0400

    chore: initial commit with github actions and basic test

---
+++ tests/test_sample.py
@@ -0,0 +1,7 @@
+# Test edəcəyimiz sadə bir toplama funksiyası
+def add(a, b):
+    return a + b
+
+# Uğurla keçməli olan test funksiyası (Adı mütləq test_ ilə başlamalıdır)
+def test_add_success():
+    assert add(10, 5) == 15
